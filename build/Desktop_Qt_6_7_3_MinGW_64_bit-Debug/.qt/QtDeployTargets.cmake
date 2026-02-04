3e16880a22e8b62a1dff736365bd11eb7d047a15
Aziz-crypto7 <azriaziz091@gmail.com>
2026-02-05 00:18:31 +0100

    Ignore build and cache files

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_sans_titre_FILE D:/projet qt/sans_titre/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/sans_titre.exe)
+set(__QT_DEPLOY_TARGET_sans_titre_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_sans_titre_RUNTIME_DLLS D:/QT/6.7.3/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.3/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.3/mingw_64/bin/Qt6Core.dll)
