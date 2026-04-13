320fcc2404aab9bde0ddb0adb37f36cadc0f7574
Yoserm <Yosser.Mofti@esprit.tn>
2026-04-13 20:41:29 +0100

    idk

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/deploy_gestion_peche_d2c6379b6c.cmake
@@ -0,0 +1,8 @@
+include(C:/Users/yoser/OneDrive/Documents/GitHub/projetcpp2526-s2-2a14-Smart-fishing-Port-Management/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/gestion_peche-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_ALL_MODULES_FOUND_VIA_FIND_PACKAGE "ZlibPrivate;EntryPointPrivate;Core;Gui;Widgets")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE C:/Users/yoser/OneDrive/Documents/GitHub/projetcpp2526-s2-2a14-Smart-fishing-Port-Management/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/gestion_peche.exe
+    GENERATE_QT_CONF
+)
